d10243f34c54940004bd294a03c7b45af31fb130
adelorenzo <adolfo@delorenzo.mobi>
2020-05-08 08:21:19 -0600

    Update ads-block.sh

--- ads-block.sh
+++ ads-block.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-rm hosts*
-wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
-echo "server:" > ads.conf
-cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 10.51.1.124\""}' >> ads.conf
-cp ads.conf /etc/unbound/unbound.conf.d/ads.conf
+rm /tmp/hosts*
+wget -O /tmp/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
+echo "server:" > /tmp/ads.conf
+cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 10.51.1.124\""}' >> /tmp/ads.conf
+cp /tmp/ads.conf /etc/unbound/unbound.conf.d/ads.conf
 service unbound restart
